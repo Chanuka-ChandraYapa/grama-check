2d82febd4fe8e5f9d41e08219f8f546d9dc61e3f
thiva-k <kalyanasundaramthivaharan@gmail.com>
2023-12-07 00:51:16 +0530

    Changed deployed url

--- src/main.tsx
+++ src/main.tsx
@@ -11,9 +11,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         // signInRedirectURL: "http://localhost:3000",
         // signOutRedirectURL: "http://localhost:3000",
         signInRedirectURL:
-          "https://ec540c3d-52b5-4f95-8be2-e30f22cbdbc7.e1-us-east-azure.choreoapps.dev",
+          "https://5399b3e1-b549-4093-92cc-4e20805a9383.e1-us-east-azure.choreoapps.dev",
         signOutRedirectURL:
-          "https://ec540c3d-52b5-4f95-8be2-e30f22cbdbc7.e1-us-east-azure.choreoapps.dev",
+          "https://5399b3e1-b549-4093-92cc-4e20805a9383.e1-us-east-azure.choreoapps.dev",
         clientID: "ufGfDEvOxuCRhDPL15fk1KFQHBQa",
         baseUrl: "https://api.asgardeo.io/t/thivaorg",
         scope: ["openid", "profile"],
