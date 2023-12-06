a529881e64693d252e4296daf70bc1be32221d15
thiva-k <kalyanasundaramthivaharan@gmail.com>
2023-12-07 00:44:44 +0530

    baby integration

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,17 +1,46 @@
 import React, { useState } from "react";
-import { ApiResponse, submitFormData } from "../api/IdCheckApi";
+
+import { useAuthContext } from "@asgardeo/auth-react";
 
 const Form: React.FC = () => {
   const [id, setId] = useState("");
   const [address, setAddress] = useState("");
+  const { getAccessToken } = useAuthContext();
 
-  const handleSubmit = async () => {
-    try {
-      const response: ApiResponse = await submitFormData(id);
-      console.log("API Response:", response);
+  const handleSubmit = () => {
+    // Call getAccessToken, which returns a Promise
+    getAccessToken()
+      .then((token) => {
+        // Token is the resolved value of the promise
+        console.log("Access Token:", token);
+        const apiUrl = `https://7902e7c7-f73b-401f-a1db-07c524deb30a-prod.e1-us-east-azure.choreoapis.dev/rkjj/id-check/endpoint-9090-803/v1/checkNic/${id}`;
 
-    } catch (error) {
-    }
+        // Make the API request with the obtained access token
+        return fetch(apiUrl, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            accept: "application/json",
+          },
+        });
+      })
+      .then((response) => {
+        // Check if the API request was successful
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        // Parse the JSON response
+        return response.json();
+      })
+      .then((data) => {
+        // Handle the data from the API response
+        console.log("API Response:", data);
+      })
+      .catch((error) => {
+        // Handle errors
+        console.error("Error:", error.message);
+      });
   };
   return (
     <form
--- src/main.tsx
+++ src/main.tsx
@@ -11,10 +11,10 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         // signInRedirectURL: "http://localhost:3000",
         // signOutRedirectURL: "http://localhost:3000",
         signInRedirectURL:
-          "https://ec540c3d-52b5-4f95-8be2-e30f22cbdbc7.e1-us-east-azure.choreoapps.dev",
+          "http://localhost:3000",
         signOutRedirectURL:
-          "https://ec540c3d-52b5-4f95-8be2-e30f22cbdbc7.e1-us-east-azure.choreoapps.dev",
-        clientID: "6ZX7RT5D7yFhefksvrk4g9O0EEAa",
+          "http://localhost:3000",
+        clientID: "ufGfDEvOxuCRhDPL15fk1KFQHBQa",
         baseUrl: "https://api.asgardeo.io/t/thivaorg",
         scope: ["openid", "profile"],
       }}
