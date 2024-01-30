7224c2b19a3b3900d59ed75d0d36c910fd3814c3
Chanuka-ChandraYapa <chanukachandrayapa@gmail.com>
2024-01-30 07:49:29 +0530

    minor fix

--- src/components/statusbox.tsx
+++ src/components/statusbox.tsx
@@ -18,11 +18,11 @@ interface StatusBoxProps {
   serror: boolean;
 }
 const initialProfileData = {
-  fullName: "John Doe",
-  phoneNumber: "123-456-7890",
-  nicNumber: "123456789X",
-  gramaDivision: "Example Grama",
-  address: "123 Main Street, City",
+  fullName: "",
+  phoneNumber:"",
+  nicNumber: "",
+  gramaDivision: "",
+  address: "",
 };
 
 const StatusBox: React.FC<StatusBoxProps> = ({
