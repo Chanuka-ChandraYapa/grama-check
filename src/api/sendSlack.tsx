5b1c581b97db7a9a8300f5c783bb5c190fff970f
thiva-k <kalyanasundaramthivaharan@gmail.com>
2024-01-30 08:17:42 +0530

    Change prod urls

--- src/api/sendSlack.tsx
+++ src/api/sendSlack.tsx
@@ -1,6 +1,6 @@
 export const performSendSlack = async (token: string, nic: string, message: string) => {
     const SendSlackApiUrl =
-      "https://cf3a4176-54c9-4547-bcd6-c6fe400ad0d8-dev.e1-us-east-azure.choreoapis.dev/gich/slackservice/endpoint-7070-070/v1.0/sendNotifications";
+      "https://cf3a4176-54c9-4547-bcd6-c6fe400ad0d8-prod.e1-us-east-azure.choreoapis.dev/gich/slackservice/endpoint-7070-070/v1.0/sendNotifications";
     try {
       const SendSlackResponse = await fetch(SendSlackApiUrl, {
         method: "POST",
@@ -21,9 +21,7 @@ export const performSendSlack = async (token: string, nic: string, message: stri
         const SendSlackData = JSON.parse(responseData);
         console.log("decoupledSlackapi", SendSlackData);
         return SendSlackData;
-      } else {
-        
-      }
+      } else { /* empty */ }
     } catch (error) {
       console.error("Error in Slack check:", error);
       throw error;
