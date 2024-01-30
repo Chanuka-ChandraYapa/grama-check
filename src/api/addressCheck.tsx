60e9f288f7954c9b62e27ba85bbe6fac27035d91
Chanuka-ChandraYapa <chanukachandrayapa@gmail.com>
2024-01-30 09:27:50 +0530

    final change we are not touching this again

--- src/api/addressCheck.tsx
+++ src/api/addressCheck.tsx
@@ -1,7 +1,7 @@
 export const performAddressCheck = async (token: string, nic: string, land_no:string, street_name: string, grama_division_no: string) => {
   const addressCheckApiUrl =
     // "https://cf3a4176-54c9-4547-bcd6-c6fe400ad0d8-prod.e1-us-east-azure.choreoapis.dev/gich/address-check/endpoint-3000-197/v1.0/addressCheck";
-     "https://cf3a4176-54c9-4547-bcd6-c6fe400ad0d8-prod.e1-us-east-azure.choreoapis.dev/gich/gramadataservice/endpoint-3000-197/v1.0/addressCheck"
+     "https://cf3a4176-54c9-4547-bcd6-c6fe400ad0d8-prod.e1-us-east-azure.choreoapis.dev/gich/address-check/endpoint-3000-197/v1.0/addressCheck"
   try {
     const addressCheckResponse = await fetch(addressCheckApiUrl, {
       method: "POST",
