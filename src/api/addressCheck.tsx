2db00553b9bfe78499139e56105cad22a19b249c
Chanuka-ChandraYapa <chanukachandrayapa@gmail.com>
2024-01-30 10:15:47 +0530

    demote to dev

--- src/api/addressCheck.tsx
+++ src/api/addressCheck.tsx
@@ -1,8 +1,9 @@
 export const performAddressCheck = async (token: string, nic: string, land_no:string, street_name: string, grama_division_no: string) => {
   const addressCheckApiUrl =
     // "https://cf3a4176-54c9-4547-bcd6-c6fe400ad0d8-prod.e1-us-east-azure.choreoapis.dev/gich/address-check/endpoint-3000-197/v1.0/addressCheck";
-     "https://cf3a4176-54c9-4547-bcd6-c6fe400ad0d8-prod.e1-us-east-azure.choreoapis.dev/gich/address-check/endpoint-3000-197/v1/addressCheck"
+    //  "https://cf3a4176-54c9-4547-bcd6-c6fe400ad0d8-prod.e1-us-east-azure.choreoapis.dev/gich/address-check/endpoint-3000-197/v1/addressCheck"
     //  https://cf3a4176-54c9-4547-bcd6-c6fe400ad0d8-prod.e1-us-east-azure.choreoapis.dev/gich/address-check/endpoint-3000-197/v1
+    "https://cf3a4176-54c9-4547-bcd6-c6fe400ad0d8-dev.e1-us-east-azure.choreoapis.dev/gich/address-check/endpoint-3000-197/v1/addressCheck"
      
   try {
     console.log("Before calling the Address check api");
