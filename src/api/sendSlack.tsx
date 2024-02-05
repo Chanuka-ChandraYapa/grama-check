6648f9c36d26bca32c0620199841f683670c6550
thiva-k <kalyanasundaramthivaharan@gmail.com>
2024-02-05 18:32:43 +0530

    Update slack twilio prod url

--- src/api/sendSlack.tsx
+++ src/api/sendSlack.tsx
@@ -1,6 +1,6 @@
 export const performSendSlack = async (token: string, nic: string, message: string) => {
     const SendSlackApiUrl =
-      "https://cf3a4176-54c9-4547-bcd6-c6fe400ad0d8-prod.e1-us-east-azure.choreoapis.dev/gich/slackservice/endpoint-7070-070/v1.0/sendNotifications";
+      window.config.api + "/slackservice/endpoint-7070-070/v1.0/sendNotifications";
     try {
       const SendSlackResponse = await fetch(SendSlackApiUrl, {
         method: "POST",
